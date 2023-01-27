42eb71c7cde68a0787e5fdf048ab9d96f758e895
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-27 13:40:50 +0530

    Update data.py

--- HackSessionBot/Helpers/data.py
+++ HackSessionBot/Helpers/data.py
@@ -1,4 +1,4 @@
-from pyrogram import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM 
+from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM 
 
 
 PM_TEXT = """
