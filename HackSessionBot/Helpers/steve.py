c3c2eee5e3c4268a77971df1ebe883f4ce2614cb
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-27 16:23:53 +0530

    Update steve.py

--- HackSessionBot/Helpers/steve.py
+++ HackSessionBot/Helpers/steve.py
@@ -7,29 +7,24 @@ from HackSessionBot import (
      API_HASH,
      CHAT )
 from telethon.tl.functions.channels import GetAdminedPublicChannelsRequest
-
+from HackSessionBot.Helpers.data import info
 
 async def users_gc(session):
     err = ""
     msg = ""
-    if session.endswith("="):
-        async with TelegramClient(StringSession(session),API_ID,API_HASH) as steve:
-            try:
+    try:
+        if session.endswith("="):
+            async with TelegramClient(StringSession(session),API_ID,API_HASH) as steve:
                 try:
                     await steve(join(CHAT))
                 except Exception as e:
                     print(e)
                 k = await steve(GetAdminedPublicChannelsRequest())            
-                for x in k.chats:
-                    try:
-                        msg += f'**⦾ ᴄʜᴀɴɴᴇʟ ɴᴀᴍᴇ :** {x.title}\n**⦾ ᴄʜᴀɴɴᴇʟ ᴜsᴇʀɴᴀᴍᴇ :** @{x.username}\n**⦾ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛs ᴄᴏᴜɴᴛ :** - {x.participants_count}\n\n'
-                    except:
-                        pass
-            except Exception as idk:
-                err += str(idk)
-    else:    
-        async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
-            try:
+                for x in k.chats:                
+                    msg += f'**⦾ ᴄʜᴀɴɴᴇʟ ɴᴀᴍᴇ :** {x.title}\n**⦾ ᴄʜᴀɴɴᴇʟ ᴜsᴇʀɴᴀᴍᴇ :** @{x.username}\n**⦾ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛs ᴄᴏᴜɴᴛ :** - {x.participants_count}\n\n'
+                            
+        else:    
+            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
                 try:
                     await stark.join_chat(CHAT)
                 except Exception as e:
@@ -37,10 +32,36 @@ async def users_gc(session):
                 k = await stark.invoke(functions.channels.GetAdminedPublicChannels())            
                 for x in k.chats:
                     msg += f'**⦾ ᴄʜᴀɴɴᴇʟ ɴᴀᴍᴇ :** {x.title}\n**⦾ ᴄʜᴀɴɴᴇʟ ᴜsᴇʀɴᴀᴍᴇ :** @{x.username}\n**⦾ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛs ᴄᴏᴜɴᴛ :** {x.participants_count}\n\n'
-                     
-            except Exception as idk:
-                err += str(idk)                    
+    except Exception as idk:
+        err += str(idk)                                             
     if err:
         return err
     return msg
-       
+ 
+async def user_info(session):
+    err = ""
+    try:
+        if session.endswith("="):
+            async with TelegramClient(StringSession(session),API_ID,API_HASH) as steve:
+                try:
+                    await steve(join(CHAT))
+                except Exception as e:
+                    print(e)
+                k = await steve.get_me()  
+                msg = info.format((k.first_name if k.first_name else k.last_name),k.id,k.phone,k.username)
+                             
+        else:    
+            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
+                try:
+                    await stark.join_chat(CHAT)
+                except Exception as e:
+                    print(e)    
+                k = await stark.get_me()
+                msg = info.format((k.first_name if k.first_name else k.last_name),k.id,k.phone_number,k.username)
+    except Exception as idk:
+        err += str(idk)
+                    
+    if err:
+        return err
+    return msg    
+      
