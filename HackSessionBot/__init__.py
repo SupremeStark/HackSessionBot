7b3bab81db22d8e3bcadf904b92891b3c1a990bf
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-27 12:34:27 +0530

    Update __init__.py

--- HackSessionBot/__init__.py
+++ HackSessionBot/__init__.py
@@ -1,1 +1,8 @@
+from config import Config
 
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+TOKEN = Config.TOKEN
+OWNER_ID = Config.OWNER_ID
+START_PIC = Config.START_PIC
+MONGO_DB = Config.MONGO_DB_URL
