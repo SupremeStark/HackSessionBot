cc3642614b6337465724277631d3c4dba89ca0c5
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-27 13:48:40 +0530

    Update start.py

--- HackSessionBot/modules/start.py
+++ HackSessionBot/modules/start.py
@@ -1,6 +1,7 @@
 from pyrogram import filters
 from HackSessionBot import app , START_PIC
-from HackSessionBot.Helpers.data import PM_TEXT,PM_BUTTON
+from HackSessionBot.Helpers.data import PM_TEXT,PM_BUTTON,HACK_MODS,HACK_TEXT
+from pyrogram.types import CallbackQuery
 
 
 @app.on_message(filters.command("start") & filters.private)
@@ -15,10 +16,11 @@ async def _start(_, message):
 
 @app.on_message(filters.command("hack") & filters.private)
 async def _hack(_, message):
-    user = message.from_user.mention
-    bot = (await _.get_me()).mention 
-    await message.reply_photo(
-       photo = START_PIC,
-       caption = HACK_TEXT.format(user, bot),
-       reply_markup = PM_BUTTON) 
+    await message.reply_text(HACK_TEXT
+              reply_markup = HACK_MODS) 
+
+
+@app.on_callback_query(filters.regex("hack_btn"))
+async def heck_callback(bot : app, query : CallbackQuery):
+    await query.message.delete()
 
