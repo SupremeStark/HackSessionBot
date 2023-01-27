4966b645fbfbdffbe854202235c4c256bc13c964
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-27 13:23:05 +0530

    Create start.py

---
+++ HackSessionBot/modules/start.py
@@ -0,0 +1,9 @@
+from pyrogram import filters
+from HackSessionBot import app , START_PIC
+from HackSessionBot 
+
+
+@app.on_message(filters.command("start") & filters.private)
+async def _start(_, message)
+
+
