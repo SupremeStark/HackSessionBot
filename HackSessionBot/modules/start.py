0e52160075803e78b23bfe768a4302cf7285507d
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-27 13:34:58 +0530

    Update start.py

--- HackSessionBot/modules/start.py
+++ HackSessionBot/modules/start.py
@@ -1,9 +1,14 @@
 from pyrogram import filters
 from HackSessionBot import app , START_PIC
-from HackSessionBot 
+from HackSessionBot.Helpers.data import PM_TEXT,PM_BUTTON
 
 
 @app.on_message(filters.command("start") & filters.private)
 async def _start(_, message)
-
+    user = message.from_user.mention
+    bot = (await _.get_me()).mention 
+    await message.reply_photo(
+       photo = START_PIC,
+       caption = PM_TEXT.format(user, bot),
+       reply_markup = PM_BUTTON) 
 
