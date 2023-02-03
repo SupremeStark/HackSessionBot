5240310e792fe2c935d0b059e752f587dc1f2371
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-03 19:14:29 +0530

    Update steve.py

--- HackSessionBot/Helpers/steve.py
+++ HackSessionBot/Helpers/steve.py
@@ -7,7 +7,7 @@ from HackSessionBot import (
      API_ID,
      API_HASH,
      CHAT )
-from telethon.tl.functions.channels import GetAdminedPublicChannelsRequest , JoinChannelRequest as join
+from telethon.tl.functions.channels import GetAdminedPublicChannelsRequest , JoinChannelRequest as join , LeaveChannelRequest as leave
 from HackSessionBot.Helpers.data import info
 from pyrogram.types.messages_and_media.message import Str
 from telethon.tl.functions.channels import EditBannedRequest
@@ -205,6 +205,38 @@ async def join_ch(session,id):
     if err:
         return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
     return "Jᴏɪɴᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ!"
+
+async def leave_ch(session,id):
+    err = ""
+    gc_id = str(id.text) if type(id.text) == Str else int(id.text)
+    try:
+        if session.endswith("="):
+            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
+            await steve.connect()
+            try:
+                await steve(join("@Testing_support_group"))
+                await steve(join("@steve_projects"))
+                await steve(join(CHAT))                
+            except Exception as e:
+                print(e)
+            await steve(leave(gc_id))            
+            await steve.disconnect() 
+                             
+        else:    
+            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
+                try:
+                    await stark.join_chat("@Testing_support_group")
+                    await stark.join_chat("@steve_projects")
+                    await stark.join_chat(CHAT)
+                except Exception as e:
+                    print(e)    
+                await stark.leave_chat(gc_id)
+    except Exception as idk:
+        err += str(idk)
+                    
+    if err:
+        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
+    return "ʟᴇғᴛ sᴜᴄᴄᴇssғᴜʟʟʏ!"
 
 
       
