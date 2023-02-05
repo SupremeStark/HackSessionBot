1313ab3cd822991deceb43296f217b3bcd0d6b53
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-05 15:43:16 +0530

    Update __init__.py

--- HackSessionBot/__init__.py
+++ HackSessionBot/__init__.py
@@ -5,7 +5,6 @@ from config import Config
 from pyrogram import Client
 from rich.console import Console
 from rich.table import Table
-from motor.motor_asyncio import AsyncIOMotorClient
 from HackSessionBot.Helpers.data import LOG_TEXT,ART
 from pyromod import listen 
 
@@ -13,10 +12,9 @@ from pyromod import listen
 API_ID = Config.API_ID
 API_HASH = Config.API_HASH
 TOKEN = Config.TOKEN
-OWNER_ID = Config.OWNER_ID
 START_PIC = Config.START_PIC
 CHAT = Config.CHAT
-MONGO_DB = Config.MONGO_DB_URL
+
 
 if not START_PIC:
     START_PIC = "https://graph.org/file/864483e9fb1cec38b67fe.jpg"
@@ -34,12 +32,7 @@ app = Client(
     api_hash = API_HASH,
     bot_token = TOKEN )
     
-#db
-if MONGO_DB:
-        mongo = AsyncIOMotorClient(MONGO_DB)
-        db = mongo.BSDK
-else:
-    db = None 
+
 
 async def HackSessionBot():
     os.system("clear")
@@ -47,10 +40,6 @@ async def HackSessionBot():
     header.add_column(LOG_TEXT)
     LOG.print(header)
     LOG.print(f"[bold cyan]{ART}")
-    if db != None:
-        LOG.print("[bold cyan]ʏᴏᴜ ʜᴀᴠᴇ ᴀᴅᴅᴇᴅ ᴍᴏɴɢᴏ ᴅʙ ᴜʀʟ ɴᴏᴡ ʏᴏᴜ ᴜsᴇ ɪᴛ ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ.")
-    else:
-        LOG.print("[bold red]ʏᴏᴜ ʜᴀᴠᴇɴ'ᴛ ᴀᴅᴅᴇᴅ ᴀ ᴍᴏɴɢᴏ ᴜʀʟ Sᴏ ᴛʜᴇ ʙᴏᴛ ᴡɪʟʟ ᴡᴏʀᴋ ʙᴜᴛ ʏᴏᴜ ᴄᴀɴ'ᴛ ᴜsᴇ ʙʀᴏᴀᴅᴄᴀsᴛ ᴍᴏᴅᴜʟᴇ")
     LOG.print("[bold yellow]sᴛᴀʀᴛɪɴɢ ʏᴏᴜʀ ʙᴏᴛ ɴᴏᴡ.......")
     await app.start()    
     
