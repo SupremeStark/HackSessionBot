f982550d518c274eb3ba09ae89e9a67dfb3ebdb9
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-02-05 15:42:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 class Config:
-    API_ID = 14817849
-    API_HASH = "f72b972aadabef20c7ea193362f7c7ae"
-    TOKEN = "5881232060:AAEJEkYtfNNcEEy4FWUyECCCoLy6OJgYFKQ"  
-    START_PIC = ""
-    MONGO_DB_URL  = ""
-    OWNER_ID =5459540851 
-    CHAT = "@NandhaSupport"    
+    API_ID = 
+    API_HASH = ""
+    TOKEN = ""  
+    START_PIC = "" 
+    CHAT = ""    
